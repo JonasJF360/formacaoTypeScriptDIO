c456a34f8e7a1259fc31c828870c83e801f12720
JonasJF360 <jonasjf360@gmail.com>
2023-11-19 20:10:30 -0400

    Upload and updates

---
+++ 03_Introducao_React/my-app-ts/src/App.tsx
@@ -0,0 +1,19 @@
+import { Card } from './components/Card';
+
+function App() {
+    return (
+        <>
+            <div>
+                Helo world
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            <div>
+                Footer
+            </div>
+        </>
+    );
+}
+
+export default App;
