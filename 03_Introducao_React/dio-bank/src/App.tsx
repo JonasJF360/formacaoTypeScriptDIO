6f37506eaca277bfb9cafbb0a5b73c2a098b610f
JonasJF360 <jonasjf360@gmail.com>
2023-11-22 07:32:28 -0400

    Upload end update files

--- 03_Introducao_React/dio-bank/src/App.tsx
+++ 03_Introducao_React/dio-bank/src/App.tsx
@@ -1,25 +1,24 @@
 import { Layout } from './components/Layout';
-import styled from 'styled-components';
 import {
   ChakraProvider,
   Input,
+  Box,
   Button,
-  ButtonGroup
 } from '@chakra-ui/react'
-
-const Box = styled.div`
-  background-color: orange;
-`;
 
 function App() {
   return (
     <ChakraProvider>
-      <h1>Faça o login</h1>
-      <Input placeholder="Email" type='email'/>
-      <Input placeholder="Senha" type='password'/>
-      <Button colorScheme='teal' variant='outline'>
-        Entrar
-      </Button>
+      <Box display='flex' justifyContent='center' alignItems='start' minHeight='100vh' backgroundColor='#9413dc' padding='10px'>
+        <Box backgroundColor="#fff" maxWidth='450px' padding='10px' borderRadius='15px'>
+          <h1>Faça o login</h1>
+          <Input placeholder="Email" type='email'/>
+          <Input placeholder="Senha" type='password' marginTop='10px' />
+          <Button colorScheme='teal' size='sm' width='100%' marginTop='10px'>
+            Entrar
+          </Button>
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
